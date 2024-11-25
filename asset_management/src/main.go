3d86e8132ab39bce2339d111f8f9b1c4e0dae062
stefanovic94 <steffen.nielsen.1994@gmail.com>
2024-11-25 13:04:21 +0100

    Add rate limiting and graceful shutdown to main server
    Implemented rate limiting middleware to control the number of requests per IP. Included a graceful shutdown mechanism for the server to handle termination signals, ensuring proper resource cleanup and logging throughout the shutdown process.

--- asset_management/src/main.go
+++ asset_management/src/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"asset_management/src/application/controllers/rest/assets"
+	"asset_management/src/configs"
+	"context"
+	"errors"
+	"github.com/JGLTechnologies/gin-rate-limit"
 	"github.com/gin-gonic/gin"
-	"log"
-
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 	router := gin.Default()
+
+	// Define here how many requests per second per IP
+	store := ratelimit.InMemoryStore(&ratelimit.InMemoryOptions{
+		Rate:  time.Second,
+		Limit: 1,
+	})
+	rateLimitMiddleware := ratelimit.RateLimiter(store, &ratelimit.Options{
+		ErrorHandler: func(c *gin.Context, info ratelimit.Info) {
+			c.JSON(http.StatusTooManyRequests, gin.H{
+				"status": "Rate limit exceeded",
+			})
+		},
+		KeyFunc: func(c *gin.Context) string {
+			return c.ClientIP()
+		},
+	})
+	router.Use(rateLimitMiddleware)
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
@@ -16,5 +40,41 @@ func main() {
 		})
 	})
 
-	log.Fatal(router.Run(":8000"))
+	assets.AssetRouter(router.Group("/assets"))
+
+	srv := &http.Server{
+		Addr:    ":8000",
+		Handler: router.Handler(),
+	}
+
+	logger := configs.Logger()
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Panic().Err(err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall. SIGKILL but can't be caught, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Info().Msg("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Info().Err(err).Msg("Server shutting down...")
+	}
+	// catching ctx.Done(). timeout of 5 seconds.
+	select {
+	case <-ctx.Done():
+		logger.Info().Msg("timeout of 5 seconds.")
+	}
+	logger.Info().Msg("Server exiting")
 }
