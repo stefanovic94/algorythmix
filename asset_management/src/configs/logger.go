846553cf58c6f264222d95a7f4c570b917dc5918
stefanovic94 <steffen.nielsen.1994@gmail.com>
2024-11-25 13:04:04 +0100

    Add logger initialization function
    Implemented a new Logger function in logger.go using the zerolog package. This function configures the logger to output to stdout with a Unix timestamp format.

--- asset_management/src/configs/logger.go
+++ asset_management/src/configs/logger.go
@@ -1,1 +1,14 @@
 package configs
+
+import (
+	"os"
+
+	"github.com/rs/zerolog"
+)
+
+func Logger() zerolog.Logger {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	// Create a new logger instance and configure it
+	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	return logger
+}
